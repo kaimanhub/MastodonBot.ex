4622be38b233498911e86f95d07bed2c13ba8ad0
kaimanhub <onlycoolproject@gmail.com>
2024-08-26 01:20:39 +0300

    Add handle_demand processing for Producer
    * Remove base Tesla middleware logger at least for now
    * Add Tesla retry middelware
    * Add more debug logs to see the state of pipeline

--- lib/mastodon_bot_ex/producer.ex
+++ lib/mastodon_bot_ex/producer.ex
@@ -11,20 +11,59 @@ defmodule MastodonBotEx.Producer do
     %{status: 200} =
       MastodonBotEx.StreamingClient.stream_user_notifications()
 
-    {:producer, :ok}
+    {:producer, {:queue.new(), "", 0}}
   end
 
   @impl GenStage
   @doc "Handle demand for events from consumers"
-  def handle_demand(_number_events, _stream) do
-    {:noreply, [], :ok}
+  def handle_demand(incoming_demand, {queue, acc, pending_demand}) do
+    demand = incoming_demand + pending_demand
+
+    dispatch_events(queue, demand, acc, [])
   end
 
   @impl GenStage
   @doc "Handle incoming data from the stream, split it into events"
-  def handle_info({_, {:data, data}}, _state) do
-    events = String.split(data, "\n", trim: true)
+  def handle_info({_, {:data, data}}, {queue, acc, pending_demand}) do
+    acc = acc <> data
 
-    {:noreply, events, :ok}
+    case :binary.split(acc, "\n\n") do
+      [":thump\n"] ->
+        {:noreply, [":thump"], {queue, "", pending_demand - 1}}
+
+      [":)\n"] ->
+        {:noreply, [":thump"], {queue, "", pending_demand - 1}}
+
+      [messages, extra_acc] ->
+        updated_queue =
+          Enum.reduce(String.split(messages, "\n", trim: true), queue, fn message, internal_acc ->
+            :queue.in(message, internal_acc)
+          end)
+
+        dispatch_events(updated_queue, pending_demand, extra_acc, [])
+
+      _ ->
+        {:noreply, [], {queue, acc, pending_demand}}
+    end
+  end
+
+  @impl GenStage
+  def handle_info({_, {:error, %Mint.TransportError{reason: :closed}}}, state) do
+    # TODO: need to handle handling reconnection here
+    {:noreply, [], state}
+  end
+
+  defp dispatch_events(queue, 0, acc, events) do
+    {:noreply, Enum.reverse(events), {queue, acc, 0}}
+  end
+
+  defp dispatch_events(queue, demand, acc, events) do
+    case :queue.out(queue) do
+      {{:value, event}, queue} ->
+        dispatch_events(queue, demand - 1, acc, [event | events])
+
+      {:empty, queue} ->
+        {:noreply, Enum.reverse(events), {queue, acc, demand}}
+    end
   end
 end
